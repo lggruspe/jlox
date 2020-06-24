d65ce6babae1f2766a439c6f13f0f72229547c49
Levi Gruspe <mail.levig@gmail.com>
2020-06-24 21:02:01 +0800

    Add comma operator

--- lox/Parser.java
+++ lox/Parser.java
@@ -23,7 +23,13 @@ class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        Expr expr = equality();
+        while (match(COMMA)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
     }
 
     private Expr equality() {
