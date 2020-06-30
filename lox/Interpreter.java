e4ca69f03559544e0b51c9e77d34a4236bc30784
Levi Gruspe <mail.levig@gmail.com>
2020-06-30 17:02:09 +0800

    Add functions

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -167,6 +168,29 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
         // Unreachable.
         return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+                    "Can only call functions and classes.");
+        }
+        LoxCallable function = (LoxCallable)callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren,
+                    String.format("Expected %d arguments but got %d.",
+                        function.arity(),
+                        arguments.size()));
+        }
+        return function.call(this, arguments);
     }
 
     @Override
--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -57,6 +57,7 @@ define_ast(args.output, "Expr", [
     "Ternary    : Expr left, Expr middle, Expr right",
     "Assign     : Token name, Expr value",
     "Binary     : Expr left, Token operator, Expr right",
+    "Call       : Expr callee, Token paren, List<Expr> arguments",
     "Grouping   : Expr expression",
     "Literal    : Object value",
     "Logical    : Expr left, Token operator, Expr right",
