829ac547271f7864e8e5c2dbee3b0c1cb2f37bcb
Levi Gruspe <mail.levig@gmail.com>
2020-07-05 16:55:03 +0800

    Parse classes

--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -68,6 +68,7 @@ define_ast(args.output, "Expr", [
 
 define_ast(args.output, "Stmt", [
     "Block      : List<Stmt> statements",
+    "Class      : Token name, List<Stmt.Function> methods",
     "Expression : Expr expression",
     "Function   : Token name, List<Token> params, List<Stmt> body",
     "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
