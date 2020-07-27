8e29ed4ff5cc7104ea02258f509c99cce205a20c
Levi Gruspe <mail.levig@gmail.com>
2020-07-27 18:39:06 +0800

    Show VM stack contents in debug mode

--- clox/vm.c
+++ clox/vm.c
@@ -24,18 +24,29 @@ static InterpretResult run() {
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
+                push(constant);
                 printValue(constant);
                 printf("\n");
                 break;
             }
-            case OP_RETURN:
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
+            }
         }
     }
 
