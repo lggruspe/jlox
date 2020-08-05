af9ab064eab40cdbbbfe3ae6b5589f3669eb4f7a
Levi Gruspe <mail.levig@gmail.com>
2020-08-05 22:54:30 +0800

    Add parser expression skeleton and helper code

--- clox/vm.c
+++ clox/vm.c
@@ -68,8 +68,21 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(char const* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
 
 void push(Value value) {
