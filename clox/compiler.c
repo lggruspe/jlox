84a486305fb909314a24eb4b0bf44ba3bb4b206a
Levi Gruspe <mail.levig@gmail.com>
2020-08-06 18:04:46 +0800

    Add ! operator

--- clox/compiler.c
+++ clox/compiler.c
@@ -167,6 +167,7 @@ static void unary() {
     TokenType operatorType = parser.previous.type;
     parsePrecedence(PREC_UNARY);
     switch (operatorType) {
+    case TOKEN_BANG: emitByte(OP_NOT); break;
     case TOKEN_MINUS: emitByte(OP_NEGATE); break;
     default:
         return;
@@ -185,7 +186,7 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON]     = { NULL,     NULL,   PREC_NONE   },
     [TOKEN_SLASH]         = { NULL,     binary, PREC_FACTOR },
     [TOKEN_STAR]          = { NULL,     binary, PREC_FACTOR },
-    [TOKEN_BANG]          = { NULL,     NULL,   PREC_NONE   },
+    [TOKEN_BANG]          = { unary,    NULL,   PREC_NONE   },
     [TOKEN_BANG_EQUAL]    = { NULL,     NULL,   PREC_NONE   },
     [TOKEN_EQUAL]         = { NULL,     NULL,   PREC_NONE   },
     [TOKEN_EQUAL_EQUAL]   = { NULL,     NULL,   PREC_NONE   },
