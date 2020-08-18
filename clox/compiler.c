cf9f25742bf6f0131b0a7b9576be9a86e34976dd
Levi Gruspe <mail.levig@gmail.com>
2020-08-18 23:59:24 +0800

    cc -- Disallow variable in its own initializer

--- clox/compiler.c
+++ clox/compiler.c
@@ -186,6 +186,9 @@ static int resolveLocal(Compiler* compiler, Token *name) {
     for (int i = compiler->localCount - 1; i >= 0; i--) {
         Local *local = &compiler->locals[i];
         if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("Cannot read local variable in its own initializer.");
+            }
             return i;
         }
     }
@@ -199,7 +202,7 @@ static void addLocal(Token name) {
     }
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -227,6 +230,10 @@ static uint8_t parseVariable(const char* errorMessage) {
     if (current->scopeDepth > 0) return 0;
 
     return identifierConstant(&parser.previous);
+}
+
+static void markInitialized() {
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
 static void binary(bool canAssign) {
@@ -377,6 +384,7 @@ static void parsePrecedence(Precedence precedence) {
 
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
     emitBytes(OP_DEFINE_GLOBAL, global);
