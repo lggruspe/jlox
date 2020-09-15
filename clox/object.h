610168e96872e0c193f8c5ba6630412ce7c48bf8
Levi Gruspe <mail.levig@gmail.com>
2020-09-15 16:03:14 +0800

    Add function objects

--- clox/object.h
+++ clox/object.h
@@ -2,14 +2,19 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_FUNCTION,
     OBJ_STRING,
 } ObjType;
 
@@ -18,6 +23,13 @@ struct sObj {
     struct sObj* next;
 };
 
+typedef struct {
+    Obj obj;
+    int arity;
+    Chunk chunk;
+    ObjString* name;
+} ObjFunction;
+
 struct sObjString {
     Obj obj;
     int length;
@@ -25,6 +37,7 @@ struct sObjString {
     uint32_t hash;
 };
 
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 
